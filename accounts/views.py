2f6875c5e97bc28d6cf31d70a5acd341b039630b
NikhilYadavCoder <nikhilgolla9@gmail.com>
2025-09-21 00:37:31 +0530

    customer dashboard

--- accounts/views.py
+++ accounts/views.py
@@ -9,6 +9,7 @@ from django.contrib.auth.decorators import login_required,user_passes_test
 from django.utils.http import urlsafe_base64_decode
 from django.contrib.auth.tokens import default_token_generator
 from vendor.models import Vendor
+from orders.models import Order
 
 from django.template.defaultfilters import slugify
 
@@ -172,7 +173,15 @@ def myAccount(request):
 @login_required(login_url='login')
 @user_passes_test(check_role_customer)
 def custDashboard(request):
-    return render(request,'accounts/custDashboard.html')
+    orders = Order.objects.filter(user=request.user,is_ordered=True)
+    recent_orders = orders[:5]
+    context = {
+        'orders': orders,
+        'orders_count': orders.count(),
+        'recent_orders': recent_orders,
+    }
+    return render(request,'accounts/custDashboard.html',context)
+
 
 
 @login_required(login_url='login')
